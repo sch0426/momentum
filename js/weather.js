d8cb921476a2d0f4d8263635459f7e03fcb3d26f
sch0426 <117507363+sch0426@users.noreply.github.com>
2023-01-09 05:46:30 +0900

    momentum ver1.0

---
+++ js/weather.js
@@ -0,0 +1,20 @@
+const API_KEY = "d3858bc2c07d4b52ecf3a36218ee24ed";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} 🌡️ ${data.main.temp} / `;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
